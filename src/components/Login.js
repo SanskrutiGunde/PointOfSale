41839db997d8b90fcbad055d5848ec4b1cd5ec32
Sanskruti Gunde <sanskruti.gunde@gmail.com>
2021-06-01 15:26:13 +0530

    🔼 Google Login Button

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,9 @@ const Login = (props) => (
   <nav className="login">
     <h2>Inventory Login</h2>
     <p>Sign in to manage your store's inventory</p>
+    <button className="twitter" onClick={() => props.authenticate("Google")}>
+      Log In With Google
+    </button>
     <button className="github" onClick={() => props.authenticate("Github")}>
       Log In With Github
     </button>
