a89848ca13dc4d77873ebd93aaf113d2357fcf76
Sanskruti Gunde <sanskruti.gunde@gmail.com>
2021-06-03 12:23:22 +0530

    🐞 Bug fix

--- src/base.js
+++ src/base.js
@@ -1,8 +1,14 @@
 import Rebase from "re-base";
 import firebase from "firebase";
+let ApiKey;
+if (process.env.NODE_ENV !== "production") {
+  ApiKey = process.env.REACT_APP_API_KEY;
+} else {
+  ApiKey = process.env.POS_API_KEY;
+}
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyB6zLeT5qmPATz8XPQZWECTryRk2CrgmvY",
+  apiKey: ApiKey,
   authDomain: "catch-of-the-day-sok.firebaseapp.com",
   databaseURL: "https://catch-of-the-day-sok-default-rtdb.firebaseio.com",
 });
